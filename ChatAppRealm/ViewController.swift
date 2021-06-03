4fc2ed8073a0451f6c0bf216754ba2adf915b3a0
Giuliano Soria <44099099+GiulianoSoria@users.noreply.github.com>
2021-06-03 10:11:41 -0700

    Initial Commit

---
+++ ChatAppRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppRealm
+//
+//  Created by Giuliano Soria Pazos on 2021-06-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
